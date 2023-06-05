b6426879e7a50b2c0dd724bcf9f94a9b3888e907
madahtoxa <madahtoxa@mail.ru>
2023-06-05 21:51:56 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+
 public class Main {
+    public static final String REGEX = "^[a-zA-Z0-9_]*$";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            check("Test", "123", "123");
+            System.out.println("всё ок");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void check(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (!login.matches(REGEX)) {
+            throw new WrongLoginException("некорректный логин");
+        }
+        if(login.length()>20){
+            throw new WrongLoginException("размер больше 20");
+        }
+        if(!password.matches(REGEX)){
+            throw new WrongPasswordException("некорректный пароль");
+        }
+        if(password.length()>20){
+            throw new WrongPasswordException("пароль больше 20");
+        }
+        if(!password.equals(confirmPassword)){
+            throw new WrongPasswordException("пароли не совпадают");
+        }
     }
 }
